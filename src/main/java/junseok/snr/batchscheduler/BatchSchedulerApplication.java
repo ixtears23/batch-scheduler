8a93bff7e0259f6746d7a6191a146f2f8d15cde8
junseokoh0 <junseok@bodycodi.com>
2021-09-06 22:48:36 +0900

    first commit

---
+++ src/main/java/junseok/snr/batchscheduler/BatchSchedulerApplication.java
@@ -0,0 +1,13 @@
+package junseok.snr.batchscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchSchedulerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/junseok/snr/batchscheduler/BatchSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package junseok.snr.batchscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchSchedulerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
